7e86103ef6bf8915f4594fa271a24d02899cf13d
SunsetRider <imusys@gmail.com>
2013-11-12 22:53:14 -0500

    second algorithm

--- LongestSubstringWithoutRepeatingCharacters.java
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -47,3 +47,43 @@ public class Solution {
     }
 }
 
+/*
+*  Algorithm 2:
+*  1.Track the length of substring without repeating characters while travesing the string;
+*  2.Compare the current length with max length, if it's larger than max, set it to max.
+*  Time Complexity:
+*  O(n)
+*/
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+    
+        if (s == null || s.length() == 0)
+            return 0;
+
+        List<Character> subString = new ArrayList<Character>();
+        int maxLength = 1;
+        int curentLength = 0;
+
+        for (int index = 0; index < s.length(); index++) {
+            char c = s.charAt(index);
+            if (subString.contains(c)) {
+                if (curentLength > maxLength)
+                    maxLength = curentLength;
+
+                int indexOfc = subString.indexOf(c);
+                subString = subString.subList(indexOfc + 1, subString.size());
+                curentLength -= indexOfc + 1;
+            }
+
+            subString.add(c);
+            curentLength++;
+        }
+        // compare the last substring's length with the max length
+        if (curentLength > maxLength)
+                maxLength = curentLength;
+
+        return maxLength;
+        }
+}
