3f2620511286693fc1b1caa88fb1c9df101d417e
SunsetRider <imusys@gmail.com>
2013-11-15 23:41:48 -0500

    thinking in first algorithm

---
+++ LongestPalindromicSubstring.java
@@ -0,0 +1,33 @@
+/**
+ * Problem:
+ * Given a string S, find the longest palindromic substring in S.
+ * You may assume that the maximum length of S is 1000,
+ * and there exists one unique longest palindromic substring.
+ */
+
+/**
+ * Algorithm 1:
+ * 
+ */
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        int sLength = s.length();
+        if (s == null || sLength == 1)
+        	return s;
+
+        int middleIndex = 0;
+        String pString = new String ();
+        if (sLength % 2 == 1) {
+            for (int i = 0; i < sLength/2; i++) {
+                s.
+            }
+
+        }
+        for (int i = 0; i < s.length() ; i++) {
+        	s.getCharAt(i); 
+        }
+    }
+}
