18f51677cfd6415ff26f77b36b24eb7f27083da1
SunsetRider <imusys@gmail.com>
2013-11-16 23:53:08 -0500

    longest odd palindromic substring

--- LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring.java
@@ -16,18 +16,30 @@ public class Solution {
         // the same Solution instance will be reused for each test case.
         int sLength = s.length();
         if (s == null || sLength == 1)
-        	return s;
+            return s;
 
-        int middleIndex = 0;
-        String pString = new String ();
-        if (sLength % 2 == 1) {
-            for (int i = 0; i < sLength/2; i++) {
-                s.
+        int middleIndex = sLength/2;
+        int shiftIndex;
+
+        int pSubLength = 0;
+        String oddPString = new String ();
+        for (int i = 0; i < sLength - 2; i++) {
+            middleIndex += i * Math.pow(-1, i);
+            for (shiftIndex = 1; middleIndex - shiftIndex >= 0 && middleIndex + shiftIndex < sLength; shiftIndex++) {
+                if (s.charAt(middleIndex + shiftIndex) == s.charAt(middleIndex - shiftIndex))
+                    pSubLength++;
+                else
+                    break;
             }
+            shiftIndex--;
 
+            if (pSubLength * 2 + 1 > oddPString.length() && pSubLength != 0) {
+                oddPString = s.substring(middleIndex - shiftIndex, middleIndex + shiftIndex + 1);
+                if ((sLength/2-Math.abs(sLength/2-(middleIndex+(i+1)*Math.pow(-1, i+1)))*2+1) <= oddPString.length())
+                    break;
+            }
         }
-        for (int i = 0; i < s.length() ; i++) {
-        	s.getCharAt(i); 
-        }
+
+        return oddPString;
     }
 }
