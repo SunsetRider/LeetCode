83cf2aae0d06d25d3934b267f5fbd3a3642507ee
SunsetRider <imusys@gmail.com>
2013-11-18 23:33:24 -0500

    first algorithm

--- LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring.java
@@ -7,7 +7,7 @@
 
 /**
  * Algorithm 1:
- * 
+ *
  */
 
 public class Solution {
@@ -17,14 +17,22 @@ public class Solution {
         int sLength = s.length();
         if (s == null || sLength == 1)
             return s;
+        if (sLength == 2 && s.charAt(0) == s.charAt(1))
+            return s;
 
-        int middleIndex = sLength/2;
-        int shiftIndex;
-
-        int pSubLength = 0;
         String oddPString = new String ();
+        String evenPString = new String ();
+
+        int middleIndex;
+        int shiftIndex;
+        int pSubLength;
+
+        // palindromic substring's length is odd
+        middleIndex = sLength/2;
         for (int i = 0; i < sLength - 2; i++) {
             middleIndex += i * Math.pow(-1, i);
+            pSubLength = 0;
+
             for (shiftIndex = 1; middleIndex - shiftIndex >= 0 && middleIndex + shiftIndex < sLength; shiftIndex++) {
                 if (s.charAt(middleIndex + shiftIndex) == s.charAt(middleIndex - shiftIndex))
                     pSubLength++;
@@ -33,13 +41,41 @@ public class Solution {
             }
             shiftIndex--;
 
-            if (pSubLength * 2 + 1 > oddPString.length() && pSubLength != 0) {
+            if (pSubLength * 2 + 1 > oddPString.length() && pSubLength != 0)
                 oddPString = s.substring(middleIndex - shiftIndex, middleIndex + shiftIndex + 1);
-                if ((sLength/2-Math.abs(sLength/2-(middleIndex+(i+1)*Math.pow(-1, i+1)))*2+1) <= oddPString.length())
-                    break;
-            }
+
+            if (middleIndex < sLength/2 && (middleIndex*2+1) <= oddPString.length())
+                break;
+            if (middleIndex > sLength/2 && (sLength-middleIndex)*2-1 <= oddPString.length())
+                break;
         }
 
-        return oddPString;
+        // palindromic substring's length is even
+        middleIndex = sLength/2;
+        for (int i = 0; i < sLength; i++) {
+            middleIndex += i * Math.pow(-1, i);
+            pSubLength = 0;
+
+            for (shiftIndex = 0; middleIndex - shiftIndex - 1 >= 0 && middleIndex + shiftIndex < sLength; shiftIndex++) {
+                if (s.charAt(middleIndex + shiftIndex) == s.charAt(middleIndex - 1 - shiftIndex))
+                    pSubLength++;
+                else
+                    break;
+            }
+            shiftIndex--;
+
+            if (pSubLength * 2 > evenPString.length() && pSubLength != 0)
+                evenPString = s.substring(middleIndex - 1 - shiftIndex, middleIndex + shiftIndex + 1);
+
+            if (middleIndex < sLength/2 && (middleIndex*2+2) == evenPString.length())
+                break;
+            if (middleIndex > sLength/2 && (sLength-middleIndex)*2 == evenPString.length())
+                break;
+        }
+
+        if (oddPString.length() > evenPString.length())
+            return oddPString;
+        else
+            return evenPString;
     }
 }
