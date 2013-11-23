8c92cb94bcc3bc012852ca91888aa23b82f4574a
SunsetRider <imusys@gmail.com>
2013-11-22 23:37:35 -0500

    second thought

--- StringToInteger.java
+++ StringToInteger.java
@@ -9,21 +9,21 @@
  * You are responsible to gather all the input requirements up front.
  *
  * spoilers alert...
- * 
+ *
  * Requirements for atoi:
  * The function first discards as many whitespace characters as necessary
  * until the first non-whitespace character is found. 
  * Then, starting from this character, takes an optional initial plus 
  * or minus sign followed by as many numerical digits as possible,
  * and interprets them as a numerical value.
- * 
+ *
  * The string can contain additional characters after those that form the integral number,
  * which are ignored and have no effect on the behavior of this function.
- * 
+ *
  * If the first sequence of non-whitespace characters in str is not a valid integral number,
  * or if no such sequence exists because either str is empty or it contains only whitespace characters,
  * no conversion is performed.
- * 
+ *
  * If no valid conversion could be performed, a zero value is returned.
  * If the correct value is out of the range of representable values,
  * INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
@@ -31,21 +31,42 @@
 
 /**
  * Algorithm 1:
- * 
+ *
  */
 
 public class Solution {
     public int atoi(String str) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
-        
+
         int result = 0;
         boolean negative;
-        char digits[] = new digits(){'0','1','2','3','4','5','6','7','8','9','10'};
-
+        boolean sign = false;
+        boolean digit = false;
+        int digitStartAt = 0;
         
 
+        for (int i = 0; i++ < str.length(); i++) {
+            if (str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+                digitStartAt = i;
+                break;
+            }
+            if (!sign && str.charAt(i) == ' ')
+                continue;
+            
+            if (!sign && str.charAt(i) == '-') {
+                negative = true;
+                sign = true;
+            }
+            else if (!sign && str.charAt(i) == '+') {
+                sign = true;
+                continue;
+            }
+            
+            
+        }
 
+        Integer.parseInt();
         return result;
     }
 }
