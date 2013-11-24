6b6bbb697b1d729303b90d7bff403dbc8c6589a5
SunsetRider <imusys@gmail.com>
2013-11-24 00:05:38 -0500

    second algorithm

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -37,3 +37,36 @@ public class Solution {
         return rev == x;
     }
 }
+
+/**
+ * Algorithm 2:
+ * Compare the digits from the ends to middle.
+ */
+public class Solution {
+    public boolean isPalindrome(int x) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+
+        if (x < 0)
+            return false;
+        else if (x >= 0 && x <= 9) {
+            return true;
+        }
+
+        int length = 0;
+
+        for (int n = x; n != 0; n /= 10) {
+            length++;
+        }
+
+        int num = x;
+        for (int i = 0; i < length/2; i++, num /= 10) {
+            if (x/(int)Math.pow(10, length-1-i)%10 != num%10) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
