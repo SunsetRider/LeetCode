62344ec867c0dcf2697becdd14e2774811ca0861
SunsetRider <imusys@gmail.com>
2013-12-29 15:52:52 -0500

    first algorithm

--- 3Sum.java
+++ 3Sum.java
@@ -15,37 +15,51 @@
 
 /**
  * Aglorithm 1:
- * 
+ * 1.Sort the array in ascending order;
+ * 2.Traverse the number not larger than 0 as a, then find the b c in the rest of the array such that a+b+c == 0
  */
 public class Solution {
     public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
-        Arrays.sort(num);
-        int indexStart = 0;
-        int indexEnd = num.length - 1;
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
         ArrayList<Integer> triplet;
 
-        while (indexStart < indexEnd-1) {
-            for (int i = indexStart + 1; i < indexEnd; i++) {
-                if (num[indexStart] + num[indexEnd] + num[i] == 0) {
-                    triplet = new ArrayList<Integer>();
-                    triplet.add(num[indexStart]);
-                    triplet.add(num[i]);
-                    triplet.add(num[indexEnd]);
-                    result.add(triplet);
-                    break;
-                }
+        // make sure the input array's length is not less than 3
+        if (num.length < 3) {
+            return result;
+        }
+
+        Arrays.sort(num);
+
+        // for each a not larger than 0 find respective b c
+        for (int i = 0; i < num.length - 2 && num[i] <= 0; i++) {
+            // remove duplicate a
+            if (i > 0 && num[i] == num[i-1]) {
+                continue;
             }
 
-            if (num[indexStart] + num[indexEnd] >= 0) {
-                indexEnd--;
-                while (indexStart < indexEnd-1 && num[indexEnd] == num[indexEnd + 1]) {
-                    indexEnd--;
-                }
-            } else {
-                indexStart++;
-                while (indexStart < indexEnd-1 && num[indexStart] == num[indexStart - 1]) {
+            int indexStart = i + 1;
+            int indexEnd = num.length - 1;
+            while (indexStart < indexEnd) {
+                int sum = num[i] + num[indexStart] + num[indexEnd];
+                if (sum < 0) {
                     indexStart++;
+                } else if (sum > 0) {
+                    indexEnd--;
+                } else {
+                    triplet = new ArrayList<Integer>(3);
+                    triplet.add(num[i]);
+                    triplet.add(num[indexStart]);
+                    triplet.add(num[indexEnd]);
+                    result.add(triplet);
+
+                    // remove duplicate b c
+                    do {
+                        indexStart++;
+                    } while (indexStart < indexEnd && num[indexStart] == num[indexStart-1]);
+
+                    do {
+                        indexEnd--;
+                    } while (indexStart < indexEnd && num[indexEnd] == num[indexEnd+1]);
                 }
             }
         }
